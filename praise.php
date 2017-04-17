2bbc1753e747a82687b414041e09b4d2cfa4c6e8
jcuan <957551963@qq.com>
2017-04-17 18:05:47 +0800

    fix:点赞取消限制

--- praise.php
+++ praise.php
@@ -2,14 +2,10 @@
 include './po_core.php';
 include "./connect.php";
 
-	session_start();
 
-	if(!isset($_SESSION['praise'])){
-		$praiseSql="UPDATE po_tongji SET data=data+1 WHERE id =1";
-		$dbh->exec($praiseSql);
-		$_SESSION['praise']=1;
-	}
+$praiseSql="UPDATE po_tongji SET data=data+1 WHERE id =1";
+$dbh->exec($praiseSql);
 
-	$sql = 'SELECT data FROM po_tongji WHERE id = 1';
-	$result = $dbh->query($sql)->fetch(PDO::FETCH_ASSOC);
-	echoJson(['praiseNum'=>$result['data'] ],0);
+$sql = 'SELECT data FROM po_tongji WHERE id = 1';
+$result = $dbh->query($sql)->fetch(PDO::FETCH_ASSOC);
+echoJson(['praiseNum'=>$result['data'] ],0);
